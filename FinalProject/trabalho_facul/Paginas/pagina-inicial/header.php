031bb3c74f99e47e825ef1a3c8a01ad6246ed4d0
Midaxs <ildsonjr2akt@gmail.com>
2025-05-12 16:56:55 -0300

    agr vai

---
+++ FinalProject/trabalho_facul/Paginas/pagina-inicial/header.php
@@ -0,0 +1,33 @@
+<?php
+<!-- filepath: c:\Users\ildso\OneDrive\Área de Trabalho\Atividades Facul\Lz-store\FinalProject\trabalho_facul\Pagina\pagina-inicial\header.php -->
+<header>
+    <div class="top-bar">
+        <a href="index.php">
+            <div class="logo"></div>
+        </a>
+        <div class="search-bar">
+            <input type="text" placeholder="Pesquisa">
+            <button>&#10140;</button>
+        </div>
+        <div class="top-links">
+            <a href="/FinalProject/trabalho_facul/devolucao_pedi.php">Devolução e pedidos</a>
+            <a href="/FinalProject/trabalho_facul/Paginas/login/login.php">Entre ou cadastre-se</a>
+            <a href="/FinalProject/trabalho_facul/Paginas/carrinho/carrinho.php">
+                <div class="cart"><span>0</span></div>
+            </a>
+        </div>
+    </div>
+    <div class="espaco"></div>
+    <nav>
+        <ul>
+            <li><a href="/FinalProject/trabalho_facul/all.php">Todos</a></li>
+            <li><a href="/FinalProject/trabalho_facul/mais_vend.php">Mais vendido</a></li>
+            <li><a href="/FinalProject/trabalho_facul/casa.php">Casa</a></li>
+            <li><a href="/FinalProject/trabalho_facul/cozinha.php">Cozinha</a></li>
+            <li><a href="/FinalProject/trabalho_facul/eletro.php">Eletrônicos</a></li>
+            <li><a href="/FinalProject/trabalho_facul/blz.php">Beleza</a></li>
+            <li><a href="/FinalProject/trabalho_facul/moda.php">Moda</a></li>
+            <li><a href="/FinalProject/trabalho_facul/esp.php">Esportes</a></li>
+        </ul>
+    </nav>
+</header>
